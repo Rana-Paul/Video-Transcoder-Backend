a9bef46556818af90140038a5f45fad5f83ba259
Rana-Mapscrew <mapscrew.rana@gmail.com>
2023-09-03 10:31:39 +0530

    v1

--- index.js
+++ index.js
@@ -3,33 +3,32 @@ const zip = require("express-zip");
 const bodyperser = require("body-parser");
 const expressFileUpload = require("express-fileupload");
 const cors = require("cors");
-
 const fs = require("fs");
 const path = require("path");
 const ffmpegSync = require("./utils/transcoder");
 
 const app = express();
-const port = 8000;
 
 app.use(bodyperser.urlencoded({ extended: false }));
 app.use(bodyperser.json());
 app.use(cors());
-
 app.use(expressFileUpload({}));
 
+// Main Route
 app.post("/convert", async (req, res) => {
-  if(!req.files) {
-    res.status(404).send("Please Upload a Video File")
+  if (!req.files) {
+    res.status(404).send("Please Upload a Video File");
   }
   const file = req.files.file;
   console.log(file.mimetype);
 
-  if(!(file.mimetype == 'video/mp4' || 'video/wmv' || 'video/avi')) {
-    res.status(404).send("Video Format is Not Supported")
+  if (!(file.mimetype == "video/mp4" || "video/wmv" || "video/avi")) {
+    res.status(404).send("Video Format is Not Supported");
   }
   const folderName = file.name.split(".")[0];
   const filenames = [];
 
+  // Creating a Folder
   fs.mkdir(path.join(__dirname, folderName), (err) => {
     if (err) {
       console.error(`Error creating root folder: ${err}`);
@@ -46,11 +45,13 @@ app.post("/convert", async (req, res) => {
     }
   });
 
+  // Move the file to the Folder
   file.mv(folderName + "/" + file.name, function (err) {
     if (err) return res.send(err);
     console.log("File uploaded...");
   });
 
+  // Transcoding Video File
   ffmpegSync(`./${folderName}/${file.name}`, file.name, folderName)
     .then(() => {
       return new Promise((resolve, reject) => {
@@ -66,8 +67,8 @@ app.post("/convert", async (req, res) => {
         }
       });
     })
+    // Sending Output file as a Response
     .then(() => {
-
       res.zip(
         [
           {
@@ -83,19 +84,21 @@ app.post("/convert", async (req, res) => {
             name: filenames[2],
           },
         ],
+        // After sending the response Delete the Folder
         () => {
-            fs.rmdir(
-              __dirname + "/" + folderName,
-              { recursive: true, force: true },
-              function () {
-                console.log("file deleted");
-              }
-            );
+          fs.rmdir(
+            __dirname + "/" + folderName,
+            { recursive: true, force: true },
+            function () {
+              console.log("file deleted");
+            }
+          );
         }
       );
     });
 });
 
+// Test Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -103,6 +106,7 @@ app.get("/test", (req, res) => {
   res.send("Test Route");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// Server
+app.listen(8000, () => {
+  console.log(`Example app listening on port 8000`);
 });
